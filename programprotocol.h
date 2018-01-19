d3c4d004954603d0bca386f80847c43f1ab4850b
Federico Perez <FPerez@coasa.com.ar>
2018-01-19 17:01:01 -0300

    Commit

--- programprotocol.h
+++ programprotocol.h
@@ -2,6 +2,7 @@
 #define PROGRAMPROTOCOL_H
 
 #include <QByteArray>
+#include <QString>
 #include "qglobal.h"
 
 namespace Protocol {
@@ -26,6 +27,28 @@ int headerSize()
 int dataSize()
 {
     return sizeof(quint16) + sizeof(quint8);
+}
+
+header_t deserializeHeader(char* serialized)
+{
+    QString str = serialized;
+    header_t header;
+
+    strcpy(header.validationString,str.mid(0,3).toLocal8Bit().data());
+    header.size = str.mid(3,3).toInt();
+
+    return header;
+}
+
+data_t deserializeData(char* serializedData)
+{
+    QString str = serializedData;
+    data_t data;
+
+    data.btn = str.mid(0,2).toInt();
+    data.press = str.at(2) == 'S' ? 1 : 0;
+
+    return data;
 }
 
 }
